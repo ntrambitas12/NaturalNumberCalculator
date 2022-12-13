33184dff286d90d87b935301de4dc88cd238e61f
Nicholas Trambitas <47403010+ntrambitas12@users.noreply.github.com>
2022-12-13 18:59:59 -0500

    Add files via upload

---
+++ NaturalNumberCalculator/src/NNCalcModel1.java
@@ -0,0 +1,42 @@
+import components.naturalnumber.NaturalNumber;
+import components.naturalnumber.NaturalNumber2;
+
+/**
+ * Model class.
+ *
+ * @author Nicholas Trambitas
+ */
+public final class NNCalcModel1 implements NNCalcModel {
+
+    /**
+     * Model variables.
+     */
+    private final NaturalNumber top, bottom;
+
+    /**
+     * Default constructor.
+     */
+    public NNCalcModel1() {
+        this.top = new NaturalNumber2();
+        this.bottom = new NaturalNumber2();
+    }
+
+    @Override
+    public NaturalNumber top() {
+
+        /*
+         * Return top operand
+         */
+        return this.top;
+    }
+
+    @Override
+    public NaturalNumber bottom() {
+
+        /*
+         * Return bottom operand
+         */
+        return this.bottom;
+    }
+
+}
